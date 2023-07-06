1167688bc35c986fad81177c38ee782b99464c90
Jeremy Kun <j2kun@users.noreply.github.com>
2023-07-06 10:16:56 -0700

    populate AffineFullUnroll pass contents

--- lib/Transform/Affine/AffineFullUnroll.cpp
+++ lib/Transform/Affine/AffineFullUnroll.cpp
@@ -1,10 +1,21 @@
 #include "lib/Transform/Affine/AffineFullUnroll.h"
+#include "mlir/Dialect/Affine/IR/AffineOps.h"
+#include "mlir/Dialect/Affine/LoopUtils.h"
+#include "mlir/include/mlir/Pass/Pass.h"
 
 namespace mlir {
 namespace tutorial {
 
+using mlir::affine::AffineForOp;
+using mlir::affine::loopUnrollFull;
+
 void AffineFullUnrollPass::runOnOperation() {
-  return;
+  getOperation().walk([&](AffineForOp op) {
+    if (failed(loopUnrollFull(op))) {
+      op.emitError("unrolling failed");
+      signalPassFailure();
+    }
+  });
 }
 
 } // namespace tutorial
