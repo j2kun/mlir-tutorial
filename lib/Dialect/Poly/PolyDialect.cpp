5fe1f007fb217b097f4ce4e4f5c43d676c02b347
Jeremy Kun <j2kun@users.noreply.github.com>
2023-07-14 21:54:35 -0700

    Add empty Poly dialect

---
+++ lib/Dialect/Poly/PolyDialect.cpp
@@ -0,0 +1,15 @@
+#include "lib/Dialect/Poly/PolyDialect.h"
+
+#include "lib/Dialect/Poly/PolyDialect.cpp.inc"
+
+namespace mlir {
+namespace tutorial {
+namespace poly {
+
+void PolyDialect::initialize() {
+  // This is where we will register types and operations with the dialect
+}
+
+}  // namespace poly
+}  // namespace tutorial
+}  // namespace mlir
