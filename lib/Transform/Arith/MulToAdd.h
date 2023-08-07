dc345dc38ad492218eef63cec87e18725efb9d98
Jeremy Kun <j2kun@users.noreply.github.com>
2023-08-07 15:39:22 -0700

    Migrate Affine and Arith passes to tablegen (#7)
    * add tablegen for loop unrolling passes
    
    * Migrate full unroll passes to tablegen style
    
    Separate headers and cpp files for each pass, and a Passes.h header and
    `:Passes` build target to unify them.
    
    Note none of the pass details have changed, just the boilerplate around
    the implementation details.
    
    * Update tutorial-opt for full unroll passes in tablegen
    
    * Migrate MulToAdd to tablegen

--- lib/Transform/Arith/MulToAdd.h
+++ lib/Transform/Arith/MulToAdd.h
@@ -1,26 +1,15 @@
 #ifndef LIB_TRANSFORM_ARITH_MULTOADD_H_
 #define LIB_TRANSFORM_ARITH_MULTOADD_H_
 
-#include "mlir/Dialect/Func/IR/FuncOps.h"
-#include "mlir/include/mlir/Pass/Pass.h"
+#include "mlir/Pass/Pass.h"
 
 namespace mlir {
 namespace tutorial {
 
-class MulToAddPass
-    : public PassWrapper<MulToAddPass,
-                         OperationPass<mlir::func::FuncOp>> {
-private:
-  void runOnOperation() override;
+#define GEN_PASS_DECL_MULTOADD
+#include "lib/Transform/Arith/Passes.h.inc"
 
-  StringRef getArgument() const final { return "mul-to-add"; }
+}  // namespace tutorial
+}  // namespace mlir
 
-  StringRef getDescription() const final {
-    return "Convert multiplicatoins to repeated additions";
-  }
-};
-
-} // namespace tutorial
-} // namespace mlir
-
-#endif // LIB_TRANSFORM_ARITH_MULTOADD_H_
+#endif  // LIB_TRANSFORM_ARITH_MULTOADD_H_
--- tools/tutorial-opt.cpp
+++ tools/tutorial-opt.cpp
@@ -1,5 +1,5 @@
-#include "lib/Transform/Affine/AffineFullUnroll.h"
-#include "lib/Transform/Arith/MulToAdd.h"
+#include "lib/Transform/Affine/Passes.h"
+#include "lib/Transform/Arith/Passes.h"
 #include "mlir/include/mlir/InitAllDialects.h"
 #include "mlir/include/mlir/Pass/PassManager.h"
 #include "mlir/include/mlir/Pass/PassRegistry.h"
@@ -9,9 +9,8 @@ int main(int argc, char **argv) {
   mlir::DialectRegistry registry;
   mlir::registerAllDialects(registry);
 
-  mlir::PassRegistration<mlir::tutorial::AffineFullUnrollPass>();
-  mlir::PassRegistration<mlir::tutorial::AffineFullUnrollPassAsPatternRewrite>();
-  mlir::PassRegistration<mlir::tutorial::MulToAddPass>();
+  mlir::tutorial::registerAffinePasses();
+  mlir::tutorial::registerArithPasses();
 
   return mlir::asMainReturnCode(
       mlir::MlirOptMain(argc, argv, "Tutorial Pass Driver", registry));
