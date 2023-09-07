114c12df62f88d7ecede0e6c40eeb01e2203bc32
Jeremy Kun <j2kun@users.noreply.github.com>
2023-09-06 23:17:45 -0700

    Add binary op folders

--- lib/Dialect/Poly/PolyOps.cpp
+++ lib/Dialect/Poly/PolyOps.cpp
@@ -1,5 +1,8 @@
 #include "lib/Dialect/Poly/PolyOps.h"
 
+#include "mlir/Dialect/CommonFolders.h"
+#include "llvm/Support/Debug.h"
+
 namespace mlir {
 namespace tutorial {
 namespace poly {
@@ -8,6 +11,51 @@ OpFoldResult ConstantOp::fold(ConstantOp::FoldAdaptor adaptor) {
   return adaptor.getCoefficients();
 }
 
-}  // namespace poly
-}  // namespace tutorial
-}  // namespace mlir
+OpFoldResult AddOp::fold(AddOp::FoldAdaptor adaptor) {
+  return constFoldBinaryOp<IntegerAttr, APInt>(
+      adaptor.getOperands(), [&](APInt a, APInt b) { return a + b; });
+}
+
+OpFoldResult SubOp::fold(SubOp::FoldAdaptor adaptor) {
+  return constFoldBinaryOp<IntegerAttr, APInt>(
+      adaptor.getOperands(), [&](APInt a, APInt b) { return a - b; });
+}
+
+OpFoldResult MulOp::fold(MulOp::FoldAdaptor adaptor) {
+  auto lhs = dyn_cast<DenseIntElementsAttr>(adaptor.getOperands()[0]);
+  auto rhs = dyn_cast<DenseIntElementsAttr>(adaptor.getOperands()[1]);
+
+  if (!lhs || !rhs)
+    return nullptr;
+
+  auto degree = getResult().getType().cast<PolynomialType>().getDegreeBound();
+  auto maxIndex = lhs.size() + rhs.size() - 1;
+
+  SmallVector<APInt, 8> result;
+  result.reserve(maxIndex);
+  for (int i = 0; i < maxIndex; ++i) {
+    result.push_back(APInt((*lhs.begin()).getBitWidth(), 0));
+  }
+
+  int i = 0;
+  for (auto lhsIt = lhs.value_begin<APInt>(); lhsIt != lhs.value_end<APInt>();
+       ++lhsIt) {
+    int j = 0;
+    for (auto rhsIt = rhs.value_begin<APInt>(); rhsIt != rhs.value_end<APInt>();
+         ++rhsIt) {
+      // index is modulo degree because poly's semantics are defined modulo x^N = 1.
+      result[(i + j) % degree] += *rhsIt * (*lhsIt);
+      ++j;
+    }
+    ++i;
+  }
+
+  return DenseIntElementsAttr::get(
+      RankedTensorType::get(static_cast<int64_t>(result.size()),
+                            IntegerType::get(getContext(), 32)),
+      result);
+}
+
+} // namespace poly
+} // namespace tutorial
+} // namespace mlir
