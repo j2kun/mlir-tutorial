6dea94a522d6471ded1645bb3e438b85272c6565
Jeremy Kun <j2kun@users.noreply.github.com>
2023-09-19 16:06:30 -0700

    rewrite DifferenceOfSquares in tablegen

--- lib/Dialect/Poly/PolyOps.cpp
+++ lib/Dialect/Poly/PolyOps.cpp
@@ -75,50 +75,6 @@ LogicalResult EvalOp::verify() {
                                               "integer, or a complex number");
 }
 
-// Rewrites (x^2 - y^2) as (x+y)(x-y) if x^2 and y^2 have no other uses.
-struct DifferenceOfSquares : public OpRewritePattern<SubOp> {
-  DifferenceOfSquares(mlir::MLIRContext *context)
-      : OpRewritePattern<SubOp>(context, /*benefit=*/1) {}
-
-  LogicalResult matchAndRewrite(SubOp op,
-                                PatternRewriter &rewriter) const override {
-    Value lhs = op.getOperand(0);
-    Value rhs = op.getOperand(1);
-
-    // If either arg has another use, then this rewrite is probably less
-    // efficient, because it cannot delete the mul ops.
-    if (!lhs.hasOneUse() || !rhs.hasOneUse()) {
-      return failure();
-    }
-
-    auto rhsMul = rhs.getDefiningOp<MulOp>();
-    auto lhsMul = lhs.getDefiningOp<MulOp>();
-    if (!rhsMul || !lhsMul) {
-      return failure();
-    }
-
-    bool rhsMulOpsAgree = rhsMul.getLhs() == rhsMul.getRhs();
-    bool lhsMulOpsAgree = lhsMul.getLhs() == lhsMul.getRhs();
-
-    if (!rhsMulOpsAgree || !lhsMulOpsAgree) {
-      return failure();
-    }
-
-    auto x = lhsMul.getLhs();
-    auto y = rhsMul.getLhs();
-
-    AddOp newAdd = rewriter.create<AddOp>(op.getLoc(), x, y);
-    SubOp newSub = rewriter.create<SubOp>(op.getLoc(), x, y);
-    MulOp newMul = rewriter.create<MulOp>(op.getLoc(), newAdd, newSub);
-
-    rewriter.replaceOp(op, {newMul});
-    // We don't need to remove the original ops because MLIR already has
-    // canonicalization patterns that remove unused ops.
-
-    return success();
-  }
-};
-
 void AddOp::getCanonicalizationPatterns(::mlir::RewritePatternSet &results,
                                         ::mlir::MLIRContext *context) {}
 
@@ -132,7 +88,7 @@ void MulOp::getCanonicalizationPatterns(::mlir::RewritePatternSet &results,
 
 void EvalOp::getCanonicalizationPatterns(::mlir::RewritePatternSet &results,
                                          ::mlir::MLIRContext *context) {
-  populateWithGenerated(results);
+  results.add<LiftConjThroughEval>(context);
 }
 
 }  // namespace poly
