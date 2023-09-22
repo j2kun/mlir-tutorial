3421df1e320f44841c4adc587163639b24d2d58d
Jeremy Kun <j2kun@users.noreply.github.com>
2023-09-22 15:16:30 -0700

    add poly type converter

--- lib/Conversion/PolyToStandard/PolyToStandard.cpp
+++ lib/Conversion/PolyToStandard/PolyToStandard.cpp
@@ -11,6 +11,27 @@ namespace poly {
 #define GEN_PASS_DEF_POLYTOSTANDARD
 #include "lib/Conversion/PolyToStandard/PolyToStandard.h.inc"
 
+class PolyToStandardTypeConverter : public TypeConverter {
+ public:
+  PolyToStandardTypeConverter(MLIRContext *ctx) {
+    addConversion([](Type type) { return type; });
+    addConversion([ctx](PolynomialType type) -> Type {
+      int degreeBound = type.getDegreeBound();
+      IntegerType elementTy =
+          IntegerType::get(ctx, 32, IntegerType::SignednessSemantics::Signless);
+      return RankedTensorType::get({degreeBound}, elementTy);
+    });
+
+    // We don't include any custom materialization hooks because this lowering
+    // is all done in a single pass. The dialect conversion framework works by
+    // resolving intermediate (mid-pass) type conflicts by inserting
+    // unrealized_conversion_cast ops, and only converting those to custom
+    // materializations if they persist at the end of the pass. In our case,
+    // we'd only need to use custom materializations if we split this lowering
+    // across multiple passes.
+  }
+};
+
 struct PolyToStandard : impl::PolyToStandardBase<PolyToStandard> {
   using PolyToStandardBase::PolyToStandardBase;
 
