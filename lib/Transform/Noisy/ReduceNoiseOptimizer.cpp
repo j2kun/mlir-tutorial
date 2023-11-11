3875e4e35d97e3762a736cc919954ff802e1279b
Jeremy Kun <j2kun@users.noreply.github.com>
2023-11-10 17:01:23 -0800

    add noise verification analysis check

--- lib/Transform/Noisy/ReduceNoiseOptimizer.cpp
+++ lib/Transform/Noisy/ReduceNoiseOptimizer.cpp
@@ -2,6 +2,10 @@
 
 #include "lib/Dialect/Noisy/NoisyOps.h"
 #include "lib/Dialect/Noisy/NoisyTypes.h"
+#include "mlir/include/mlir/Analysis/DataFlow/DeadCodeAnalysis.h"
+#include "mlir/include/mlir/Analysis/DataFlow/IntegerRangeAnalysis.h"
+#include "mlir/include/mlir/Analysis/DataFlowFramework.h"
+#include "mlir/include/mlir/IR/Visitors.h"
 #include "mlir/include/mlir/Pass/Pass.h"
 
 namespace mlir {
@@ -16,7 +20,49 @@ struct ReduceNoiseOptimizer
     : impl::ReduceNoiseOptimizerBase<ReduceNoiseOptimizer> {
   using ReduceNoiseOptimizerBase::ReduceNoiseOptimizerBase;
 
-  void runOnOperation() { ; }
+  void runOnOperation() {
+    Operation *module = getOperation();
+
+    // Use the int range analysis to confirm the noise is always below the
+    // maximum.
+    DataFlowSolver solver;
+    // The IntegerRangeAnalysis depends on DeadCodeAnalysis, but this
+    // dependence is not automatic and fails silently.
+    solver.load<dataflow::DeadCodeAnalysis>();
+    solver.load<dataflow::IntegerRangeAnalysis>();
+    if (failed(solver.initializeAndRun(module)))
+      signalPassFailure();
+
+    auto result = module->walk([&](Operation *op) {
+      if (!llvm::isa<noisy::AddOp, noisy::SubOp, noisy::MulOp,
+                     noisy::ReduceNoiseOp>(*op)) {
+        return WalkResult::advance();
+      }
+      const dataflow::IntegerValueRangeLattice *opRange =
+          solver.lookupState<dataflow::IntegerValueRangeLattice>(
+              op->getResult(0));
+      if (!opRange || opRange->getValue().isUninitialized()) {
+        op->emitOpError()
+            << "Found op without a set integer range; did the analysis fail?";
+        return WalkResult::interrupt();
+      }
+
+      ConstantIntRanges range = opRange->getValue().getValue();
+      if (range.umax().getZExtValue() > MAX_NOISE) {
+        op->emitOpError() << "Found op after which the noise exceeds the "
+                             "allowable maximum of "
+                          << MAX_NOISE
+                          << "; it was: " << range.umax().getZExtValue()
+                          << "\n";
+        return WalkResult::interrupt();
+      }
+
+      return WalkResult::advance();
+    });
+
+    if (result.wasInterrupted())
+      signalPassFailure();
+  }
 };
 
 } // namespace noisy
